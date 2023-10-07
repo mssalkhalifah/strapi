9be71e64664f72a47fddbfa38a486fc90cc40568
mssalkhalifah <mssalkhalifah@gmail.com>
2023-10-07 18:23:42 +0000

    feat: Added bootstrap data to pre fill the Stack table

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-'use strict';
+"use strict";
+
+const { forEach } = require("../config/middlewares");
 
 module.exports = {
   /**
@@ -16,5 +18,71 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  async bootstrap({ strapi }) {
+    const stackUID = "api::stack.stack";
+
+    let stacks = [
+      {
+        title: "OutSystems",
+        uuid: "36d52e9d-9c40-441a-89ea-8926799189bd",
+        color: "white",
+      },
+      {
+        title: "React",
+        uuid: "72152238-0386-427c-af67-fab8c9c5ed8e",
+        color: "blue",
+      },
+      {
+        title: "NodeJS",
+        uuid: "cfb821fa-f1c9-442f-80ff-8f877cc8cf96",
+        color: "green",
+      },
+      {
+        title: "NextJS",
+        uuid: "456d183a-a5c0-4dc3-816a-6de93cca6d6b",
+        color: "black",
+      },
+      {
+        title: ".NET",
+        uuid: "a4c9f3bb-2c5c-41ac-a75f-55de173b8e6e",
+        color: "purple",
+      },
+    ];
+
+    const fetchedStacks = await strapi.entityService.findMany(
+      "api::stack.stack"
+    );
+
+    // Find existing item, update it and remove it from the predefined list
+    for (const element of fetchedStacks) {
+      const item = stacks.find((stack) => stack.uuid === element.uuid);
+
+      await strapi.entityService.update(stackUID, element.id, {
+        data: {
+          name: item.title,
+          uuid: item.uuid,
+          color: item.color,
+          publishedAt:
+            element.publishedAt === null
+              ? new Date().toISOString()
+              : element.publishedAt,
+        },
+      });
+
+      stacks = stacks.filter((stack) => stack.uuid !== element.uuid);
+    }
+
+    console.log("Fetched stacks\n", fetchedStacks);
+    console.log("New stacks\n", stacks);
+
+    for (const stack of stacks) {
+      await strapi.entityService.create("api::stack.stack", {
+        data: {
+          name: stack.title,
+          uuid: stack.uuid,
+          color: stack.color,
+        },
+      });
+    }
+  },
 };
