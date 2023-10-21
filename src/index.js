3faa9e5399acc521ab67e21e0295d231c068dd35
mssalkhalifah <mssalkhalifah@gmail.com>
2023-10-21 14:53:13 +0000

    feat: Added url to each tech stack

--- src/index.js
+++ src/index.js
@@ -25,27 +25,38 @@ module.exports = {
       {
         title: "OutSystems",
         uuid: "36d52e9d-9c40-441a-89ea-8926799189bd",
-        color: "white",
+        color: "#db2400",
+        url: "https://www.outsystems.com/",
       },
       {
         title: "React",
         uuid: "72152238-0386-427c-af67-fab8c9c5ed8e",
-        color: "blue",
+        color: "#61dbfb",
+        url: "https://react.dev/",
       },
       {
         title: "NodeJS",
         uuid: "cfb821fa-f1c9-442f-80ff-8f877cc8cf96",
-        color: "green",
+        color: "#3c873a",
+        url: "https://nodejs.org/en",
       },
       {
         title: "NextJS",
         uuid: "456d183a-a5c0-4dc3-816a-6de93cca6d6b",
-        color: "black",
+        color: "#000",
+        url: "https://nextjs.org/",
       },
       {
         title: ".NET",
         uuid: "a4c9f3bb-2c5c-41ac-a75f-55de173b8e6e",
-        color: "purple",
+        color: "#512BD4",
+        url: "https://dotnet.microsoft.com/en-us/",
+      },
+      {
+        title: "PostgreSQL",
+        uuid: "8acf84c2-af2a-4dda-b848-4abce7174ab8",
+        color: "#336791",
+        url: "https://dotnet.microsoft.com/en-us/",
       },
     ];
 
@@ -62,6 +73,7 @@ module.exports = {
           name: item.title,
           uuid: item.uuid,
           color: item.color,
+          url: item.url,
           publishedAt:
             element.publishedAt === null
               ? new Date().toISOString()
@@ -80,6 +92,7 @@ module.exports = {
           name: stack.title,
           uuid: stack.uuid,
           color: stack.color,
+          url: stack.url,
         },
       });
     }
